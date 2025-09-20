6d40e1cd1df25edd2255b00f9542911e1ac63499
Gala <galarragasebastian80@gmail.com>
2025-09-20 10:57:10 -0500

    modificando controlador y permisos

--- backend/src/controllers/permisos.controller.js
+++ backend/src/controllers/permisos.controller.js
@@ -1,6 +1,6 @@
 const db = require('../config/conexion_db');
 
-class permisosController{
+class PermisosController{
     async obtenerPermisos(req,res) {
         try{
             const [permisos]= await db.query('SELECT * FROM permisos');
@@ -30,6 +30,24 @@ class permisosController{
             res.status(500).json({ error:'Error al actualizar permiso'});
         }
     }
-
-
+    async actualizarPermisos(req, res){
+        const { id } = req.params;
+        const { nombre, descripcion} = req.body;
+        try{
+            await db.query('UDPADE permisos SET nombre =?, descripcion = ? WHERE id_pemisos = ?',[nombre, descripcion,id]);
+            res.json({mensaje:'Permiso actualizado correctamente'});
+        }catch(error){
+            res.status(500).json({error:'Error al actulizar permiso'});
+        }
+    }
+    async eliminarPermiso(req, res){
+        const { id } = req.params;
+        try {
+            await db.query('DELETE FROM permisos WHERE id_permiso = ?', [id]);
+            res.json({ mensaje:'Permiso eliminado correctamente'});
+        }catch(error){
+            res.status(500).json({ error : 'Error al eliminar permiso'});
+    }
+    }
 }
+module.exports = PermisosController
